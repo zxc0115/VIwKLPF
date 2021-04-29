118270eec24f81ec5c913721828a052afaaf1d49
zxc0115 <43577498+zxc0115@users.noreply.github.com>
2021-04-29 11:41:42 +0800

    Add files via upload

---
+++ yelp/KLPF/KLPF_main.py
@@ -0,0 +1,372 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+sys.path.append('../')
+
+import os
+import time
+import datetime
+import math
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from sklearn.manifold import TSNE
+from multiprocessing import cpu_count
+
+from yelp import Yelp
+from KLPF.KLPF_model import KLPF
+from utils import linear_anneal, log_Normal_diag, log_Normal_standard
+
+
+# In[2]:
+
+
+# device configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+max_len = 64
+batch_size = 32
+splits = ['train', 'valid', 'test']
+
+# Penn TreeBank (PTB) dataset
+data_path = '../data'
+datasets = {split: Yelp(root=data_path, split=split) for split in splits}
+
+# dataloader
+dataloaders = {split: DataLoader(datasets[split],
+                                    batch_size=batch_size,
+                                    shuffle=split=='train',
+                                    num_workers=cpu_count(),
+                                    pin_memory=torch.cuda.is_available())
+                                    for split in splits}
+
+symbols = datasets['train'].symbols
+
+
+# In[3]:
+
+
+datasets['train'][0]
+
+
+# In[4]:
+
+
+len(datasets['train'].word_to_idx)
+
+
+# In[5]:
+
+
+# iaf model
+embedding_size = 300
+hidden_size = 256
+latent_dim = 32
+dropout_rate = 0.5
+model = KLPF(vocab_size=datasets['train'].vocab_size,
+               embed_size=embedding_size,
+               time_step=max_len,
+               hidden_size=hidden_size,
+               z_dim=latent_dim,
+               dropout_rate=dropout_rate,
+               bos_idx=symbols['<bos>'],
+               eos_idx=symbols['<eos>'],
+               pad_idx=symbols['<pad>'],
+               n_comb = 2
+               )
+model = model.to(device)
+
+
+# In[6]:
+
+
+# folder to save model
+if False:
+    save_path = 'iaf'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+
+# In[7]:
+
+
+# objective function
+learning_rate = 0.001
+criterion = nn.NLLLoss(size_average=False, ignore_index=symbols['<pad>'])
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# negative log likelihood
+def NLL(logp, target, length):
+    target = target[:, :torch.max(length).item()].contiguous().view(-1)
+    logp = logp.view(-1, logp.size(-1))
+    return criterion(logp, target)
+
+# KL divergence
+def KL_div(mu, logvar):
+    return -0.5 * torch.sum(1. + logvar - mu.pow(2) - logvar.exp())
+
+
+# In[17]:
+
+
+# training setting
+epoch = 20
+print_every = 50
+
+# training interface
+step = 0
+tracker = {'ELBO': [], 'NLL': [], 'KL': [], 'KL_weight': []}
+start_time = time.time()
+for ep in range(epoch):
+    # learning rate decay
+    if ep >= 10 and ep % 2 == 0:
+        learning_rate = learning_rate * 0.5
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = learning_rate
+
+    for split in splits:
+        dataloader = dataloaders[split]
+        model.train() if split == 'train' else model.eval()
+        totals = {'ELBO': 0., 'NLL': 0., 'KL': 0., 'words': 0}
+        for itr, (enc_inputs, dec_inputs, targets, lengths, labels) in enumerate(dataloader):
+            bsize = enc_inputs.size(0)
+            enc_inputs = enc_inputs.to(device)
+            dec_inputs = dec_inputs.to(device)
+            targets = targets.to(device)
+            lengths = lengths.to(device)
+            labels = labels.to(device)
+
+            # forward
+            logp, z_0, z_T, mu, logvar, labels = model(enc_inputs, dec_inputs, lengths, labels)
+            
+
+            # calculate loss
+            NLL_loss = NLL(logp, targets, lengths + 1)
+            # KL loss
+            log_p_z = log_Normal_standard(z_T, dim=1)
+            log_q_z = log_Normal_diag(z_0, mu, logvar, dim=1)
+            KL_loss = torch.sum(-(log_p_z - log_q_z))
+            KL_weight = linear_anneal(step, len(dataloaders['train']) * 10)
+            loss = (NLL_loss + KL_weight * KL_loss) / bsize         
+            
+            
+            # cumulate
+            totals['ELBO'] += loss.item() * bsize
+            totals['NLL'] += NLL_loss.item()
+            totals['KL'] += KL_loss.item()
+            totals['words'] += torch.sum(lengths).item()
+
+            # backward and optimize
+            if split == 'train':
+                step += 1
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), 5)
+                optimizer.step()
+
+                # track
+                tracker['ELBO'].append(loss.item())
+                tracker['NLL'].append(NLL_loss.item() / bsize)
+                tracker['KL'].append(KL_loss.item() / bsize)
+                tracker['KL_weight'].append(KL_weight)
+                
+                if(False):
+                    # print statistics
+                    if itr % print_every == 0 or itr + 1 == len(dataloader):
+                        print("%s Batch %04d/%04d, ELBO-Loss %.4f, "
+                              "NLL-Loss %.4f, KL-Loss %.4f, KL-Weight %.4f"
+                              % (split.upper(), itr, len(dataloader),
+                                 tracker['ELBO'][-1], tracker['NLL'][-1],
+                                 tracker['KL'][-1], tracker['KL_weight'][-1]))
+            #break
+        #break
+
+        samples = len(datasets[split])
+        print("%s Epoch %02d/%02d, ELBO %.4f, NLL %.4f, KL %.4f, PPL %.4f"
+              % (split.upper(), ep, epoch, totals['ELBO'] / samples,
+                 totals['NLL'] / samples, totals['KL'] / samples,
+                 math.exp(totals['NLL'] / totals['words'])))
+    print('\n')
+
+    #break
+    # save checkpoint
+    #checkpoint_path = os.path.join(save_path, "E%02d.pkl" % ep)
+    #torch.save(model.state_dict(), checkpoint_path)
+    #print("Model saved at %s\n" % checkpoint_path)
+end_time = time.time()
+print('Total cost time',
+      time.strftime("%H hr %M min %S sec", time.gmtime(end_time - start_time)))
+
+
+# In[9]:
+
+
+# another KL
+
+totals = {'ELBO': 0., 'NLL': 0., 'KL': 0., 'words': 0}
+with torch.no_grad():
+    model.eval()
+    for itr, (enc_inputs, dec_inputs, targets, lengths, labels) in enumerate(dataloaders['test']):
+        bsize = enc_inputs.size(0)
+        enc_inputs = enc_inputs.to(device)
+        dec_inputs = dec_inputs.to(device)
+        targets = targets.to(device)
+        lengths = lengths.to(device)
+        labels = labels.to(device)
+
+        # forward
+        logp, z_0, z_T, mu, logvar, labels = model(enc_inputs, dec_inputs, lengths, labels)
+
+        # calculate loss
+        NLL_loss = NLL(logp, targets, lengths + 1)
+        # KL loss
+        log_p_z = log_Normal_standard(z_0, dim=1)
+        log_q_z = log_Normal_diag(z_0, mu, logvar, dim=1)
+        KL_loss = torch.sum(log_q_z - log_p_z)
+        #KL_weight = linear_anneal(step, len(dataloaders['train']) * 10)
+        KL_weight = 1
+        loss = (NLL_loss + KL_weight * KL_loss) / bsize
+
+        # cumulate
+        totals['ELBO'] += loss.item() * bsize
+        totals['NLL'] += NLL_loss.item()
+        totals['KL'] += KL_loss.item()
+        totals['words'] += torch.sum(lengths).item()
+    samples = len(datasets['test'])
+    print("%s Epoch %02d/%02d, ELBO %.4f, NLL %.4f, KL %.4f, PPL %.4f"
+          % (split.upper(), ep, epoch, totals['ELBO'] / samples,
+             totals['NLL'] / samples, totals['KL'] / samples,
+             math.exp(totals['NLL'] / totals['words'])))
+
+
+# In[10]:
+
+
+# calculate au
+
+delta = 0.01
+with torch.no_grad():
+    model.eval()
+    cnt = 0
+    for itr, (enc_inputs, dec_inputs, targets, lengths, labels) in enumerate(dataloaders['test']):
+        bsize = enc_inputs.size(0)
+        enc_inputs = enc_inputs.to(device)
+        dec_inputs = dec_inputs.to(device)
+        targets = targets.to(device)
+        lengths = lengths.to(device)
+        labels = labels.to(device)
+
+        # forward
+        logp, z_0, z_T, mu, logvar, labels = model(enc_inputs, dec_inputs, lengths, labels)
+        
+        if cnt == 0:
+            mu_sum = mu.sum(dim=0, keepdim=True)
+        else:
+            mu_sum = mu_sum + mu.sum(dim=0, keepdim=True)
+        cnt += mu.size(0)
+        
+    mu_mean = mu_sum / cnt
+        
+    cnt = 0
+    for itr, (enc_inputs, dec_inputs, targets, lengths, labels) in enumerate(dataloaders['test']):
+        bsize = enc_inputs.size(0)
+        enc_inputs = enc_inputs.to(device)
+        dec_inputs = dec_inputs.to(device)
+        targets = targets.to(device)
+        lengths = lengths.to(device)
+        labels = labels.to(device)
+
+        # forward
+        logp, z_0, z_T, mu, logvar, labels = model(enc_inputs, dec_inputs, lengths, labels)
+        
+        if cnt == 0:
+            var_sum = ((mu - mu_mean) ** 2).sum(dim=0)
+        else:
+            var_sum = var_sum + ((mu - mu_mean) ** 2).sum(dim=0)
+        cnt += mu.size(0)
+        
+    au_var = var_sum / (cnt - 1)
+    
+    print((au_var >= delta).sum().item())
+    #print(au_var)
+
+
+# In[11]:
+
+
+# plot KL curve
+if(True):
+    fig, ax1 = plt.subplots()
+    lns1 = ax1.plot(tracker['KL_weight'], 'b', label='KL term weight')
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.set_xlabel('Step')
+    ax1.set_ylabel('KL term weight')
+    ax2 = ax1.twinx()
+    lns2 = ax2.plot(tracker['KL'], 'r', label='KL term value')
+    ax2.set_ylabel('KL term value')
+    lns = lns1 + lns2
+    labs = [l.get_label() for l in lns]
+    ax1.legend(lns, labs, bbox_to_anchor=(0., 1.02, 1., .102),
+               ncol=2, mode="expand", borderaxespad=0.)
+    plt.show()
+
+
+# In[12]:
+
+
+# latent space visualization
+if(True):
+    features = np.empty([len(datasets['test']), latent_dim])
+    feat_label = np.empty(len(datasets['test']))
+    for itr, (enc_inputs, dec_inputs, _, lengths, labels) in enumerate(dataloaders['test']):
+        enc_inputs = enc_inputs.to(device)
+        dec_inputs = dec_inputs.to(device)
+        lengths = lengths.to(device)
+        labels = labels.to(device)
+        
+        _, z_0, z_T, mu, _, labels = model(enc_inputs, dec_inputs, lengths, labels)
+        start, end = batch_size * itr, batch_size * (itr + 1)
+        features[start:end] = mu.data.cpu().numpy()
+        feat_label[start:end] = labels.data.cpu().numpy()
+   
+
+
+# In[13]:
+
+
+features.shape
+
+
+# In[14]:
+
+
+tsne_z = TSNE(n_components=2,perplexity=200).fit_transform(features)#early_exaggeration=100
+tracker['z'] = tsne_z
+tracker['label'] = feat_label
+
+
+# In[15]:
+
+
+plt.figure()
+for i in range(0,6,4):
+    print(i)
+    plt.scatter(tsne_z[np.where(feat_label == i), 0], tsne_z[np.where(feat_label == i), 1], s=10, alpha=0.5)
+plt.axis('equal')
+plt.show()
+
+
+# In[16]:
+
+
+# save learning results
+sio.savemat("KLPF.mat", tracker)
+
---
+++ yelp/KLPFwas/KLPFwas_model.py
@@ -0,0 +1,195 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.nn.init import xavier_normal
+from torch.nn.parameter import Parameter
+import flows
+class linIAF(nn.Module):
+    def __init__(self):
+        super(linIAF, self).__init__()
+
+    def forward(self, L, z, z_dim):
+        '''
+        :param L: batch_size (B) x latent_size^2 (L^2)
+        :param z: batch_size (B) x latent_size (L)
+        :return: z_new = L*z
+        '''
+        # L->tril(L)
+        L_matrix = L.view( -1, z_dim, z_dim ) # resize to get B x L x L
+        LTmask = torch.tril( torch.ones(z_dim, z_dim), diagonal=-1 ) # lower-triangular mask matrix (1s in lower triangular part)
+        I = torch.Tensor( torch.eye(z_dim, z_dim).expand(L_matrix.size(0), z_dim, z_dim) )
+        if torch.cuda.is_available():
+            LTmask = LTmask.cuda()
+            I = I.cuda()
+        #LTmask = torch.Tensor(LTmask)
+        LTmask = LTmask.unsqueeze(0).expand( L_matrix.size(0), z_dim, z_dim ) # 1 x L x L -> B x L x L
+        LT = torch.mul( L_matrix, LTmask ) + I # here we get a batch of lower-triangular matrices with ones on diagonal
+
+        # z_new = L * z
+        z_new = torch.bmm( LT , z.unsqueeze(2) ).squeeze(2) # B x L x L * B x L x 1 -> B x L
+
+        return z_new
+
+class combination_L(nn.Module):
+    def __init__(self):
+        super(combination_L, self).__init__()
+
+    def forward(self, L, y, n_combi, z_dim):
+        '''
+        :param L: batch_size (B) x latent_size^2 * number_combination (L^2 * C)
+        :param y: batch_size (B) x number_combination (C)
+        :return: L_combination = y * L
+        '''
+        # calculate combination of Ls
+        L_tensor = L.view( -1, z_dim**2, n_combi ) # resize to get B x L^2 x C
+        y = y.unsqueeze(1).expand(y.size(0), z_dim**2, y.size(1)) # expand to get B x L^2 x C
+        L_combination = torch.sum( L_tensor * y, 2 ).squeeze()
+        return L_combination
+
+
+
+class iafEncoder(nn.Module):
+
+    def __init__(self, vocab_size, embed_size, hidden_size, z_dim, pad_idx, sigma):
+        super(iafEncoder, self).__init__()
+        self.sigma = sigma
+        # input
+        self.embedding = nn.Embedding(vocab_size, embed_size,
+                                      padding_idx=pad_idx)
+        # RNN
+        self.rnn = nn.LSTM(embed_size, hidden_size, batch_first=True)
+        # output
+        self.output = nn.Linear(hidden_size * 2, z_dim * 2)
+        # iaf highway
+        self.highway = nn.Linear(hidden_size * 2, hidden_size)
+        
+
+    def forward(self, input_seq, length):
+        # embed input
+        embedded_input = self.embedding(input_seq)
+        embedded_input = embedded_input + torch.randn_like(embedded_input).normal_(0,self.sigma)
+
+        # RNN forward
+        pack_input = pack_padded_sequence(embedded_input, length,
+                                          batch_first=True)
+        _, (h, c) = self.rnn(pack_input)
+
+        # produce mu and logvar
+        hidden = torch.cat([h, c], dim=-1).squeeze(0)
+        mu, logvar = torch.chunk(self.output(hidden), 2, dim=-1)
+        highway = self.highway(hidden)
+
+        return mu, logvar, highway
+
+class KLPFwas(nn.Module):
+
+    def __init__(self, vocab_size, embed_size, time_step, hidden_size, z_dim,
+                 dropout_rate, bos_idx, eos_idx, pad_idx, n_comb, sigma):
+        super(KLPFwas, self).__init__()
+        self.time_step = time_step
+        self.hidden_size = hidden_size
+        self.dropout_rate = dropout_rate
+        self.bos_idx = bos_idx
+        self.eos_idx = eos_idx
+        self.pad_idx = pad_idx
+        self.number_combination = n_comb
+        self.z_dim = z_dim
+
+        # encoder
+        self.encoder = iafEncoder(vocab_size, embed_size,
+                                   hidden_size, z_dim, pad_idx, sigma)
+        # iaf
+        #self.linIAF = linIAF()
+        #self.combination_L = combination_L()
+        self.flow = flows.myIAF(z_dim, z_dim, hidden_size,1)
+        #self.encoder_y = nn.Linear( hidden_size, self.number_combination )
+        #self.encoder_L = nn.Linear( hidden_size, (z_dim**2) * self.number_combination )
+        self.softmax = nn.Softmax()
+        
+        # decoder
+        self.embedding = nn.Embedding(vocab_size, embed_size,
+                                      padding_idx=pad_idx)
+        self.init_h = nn.Linear(z_dim, hidden_size)
+        self.init_c = nn.Linear(z_dim, hidden_size)
+        self.skip = nn.Linear(z_dim, embed_size)
+        self.rnn = nn.LSTM(embed_size, hidden_size, batch_first=True)
+        # output
+        self.output = nn.Linear(hidden_size, vocab_size)
+        
+    def q_z_Flow(self, z_0, weight, h_last, mu):
+        # ccLinIAF
+        #L = self.encoder_L(h_last)
+        #y = self.softmax(self.encoder_y(h_last))
+        #L_combination = self.combination_L(L, y, self.number_combination, self.z_dim)
+        #z = self.linIAF(L_combination, z_0, self.z_dim)
+        z , _, _, _ = self.flow([z_0,weight,h_last, mu])
+        return z
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(mu)
+        return mu + std * eps
+
+    def forward(self, enc_input, dec_input, length, labels):
+        # process input
+        sorted_len, sorted_idx = torch.sort(length, descending=True)
+        enc_input = enc_input[sorted_idx]
+        dec_input = dec_input[sorted_idx]
+        labels = labels[sorted_idx]
+        
+        # encode
+        mu, logvar, h = self.encoder(enc_input, sorted_len)
+        z_0 = self.reparameterize(mu, logvar)
+        weight = torch.eye(z_0.shape[1])
+        z_T = self.q_z_Flow(z_0, weight, h, mu)
+
+        # decode
+        embedded_input = self.embedding(dec_input)
+        res = self.skip(z_T).unsqueeze(1).expand(-1,self.time_step+1,-1)
+        embedded_input = embedded_input + res
+        drop_input = F.dropout(embedded_input, p=self.dropout_rate,
+                               training=self.training)
+        pack_input = pack_padded_sequence(drop_input, sorted_len + 1,
+                                          batch_first=True)
+        h_0, c_0 = self.init_h(z_T), self.init_c(z_T)
+        hidden = (h_0.unsqueeze(0), c_0.unsqueeze(0))
+        pack_output, _ = self.rnn(pack_input, hidden)
+        output, _ = pad_packed_sequence(pack_output, batch_first=True)
+        _, reversed_idx = torch.sort(sorted_idx)
+        output = output[reversed_idx]
+
+        # project output
+        batch_size, seq_len, hidden_size = output.size()
+        logit = self.output(output.view(-1, hidden_size))
+        logp = F.log_softmax(logit, dim=-1)
+        logp = logp.view(batch_size, seq_len, -1)
+
+        return logp, z_0, z_T, mu, logvar, labels
+
+    def inference(self, z):
+        # set device
+        tensor = torch.LongTensor
+        if torch.cuda.is_available():
+            tensor = torch.cuda.LongTensor
+
+        # initialize hidden state
+        batch_size = z.size(0)
+        h_0, c_0 = self.init_h(z), self.init_c(z)
+        hidden = (h_0.unsqueeze(0), c_0.unsqueeze(0))
+
+        # RNN forward
+        symbol = tensor(batch_size, self.time_step + 1).fill_(self.pad_idx)
+        for t in range(self.time_step + 1):
+            if t == 0:
+                input_seq = tensor(batch_size, 1).fill_(self.bos_idx)
+            embedded_input = self.embedding(input_seq)
+            output, hidden = self.rnn(embedded_input, hidden)
+            logit = self.output(output)
+            _, sample = torch.topk(logit, 1, dim=-1)
+            input_seq = sample.squeeze(-1)
+            symbol[:, t] = input_seq.squeeze(-1)
+
+        return symbol
+    
